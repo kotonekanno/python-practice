085d460cd7f0838afea803ab5796fba5dd6e2c1f
Kotone Kanno <kotonekanno0414@hotmail.com>
2025-05-05 01:43:33 +0900

    Create calculator.py
    dict型を使った条件分岐の練習

---
+++ calculator.py
@@ -0,0 +1,20 @@
+def add(a,b): print(f"{a} + {b} = {a+b}")
+def sub(a,b): print(f"{a} - {b} = {a-b}")
+def mul(a,b): print(f"{a} × {b} = {a*b}")
+def div(a,b): print(f"{a} ÷ {b} = {a/b}")
+
+commands = {
+    "add": add,
+    "sub": sub,
+    "mul": mul,
+    "div": div
+}
+
+cmd = input("操作を選んでください：")
+a = int(input("1つ目の数字："))
+b = int(input("2つ目の数字："))
+
+if cmd in commands:
+    commands[cmd](a,b)
+
+else: print("不正な操作")
